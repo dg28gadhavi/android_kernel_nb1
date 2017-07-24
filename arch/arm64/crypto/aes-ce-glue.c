b68ba2ec2a0f665015704ed5ac8bd645773ceb66
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2017-07-24 11:28:10 +0100

    UPSTREAM: crypto: arm64/aes-ce-cipher - match round key endianness with generic code
    In order to be able to reuse the generic AES code as a fallback for
    situations where the NEON may not be used, update the key handling
    to match the byte order of the generic code: it stores round keys
    as sequences of 32-bit quantities rather than streams of bytes, and
    so our code needs to be updated to reflect that.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    (cherry picked from commit f402e3115e20b345bd6fbfcf463a506d958c7bf6)
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- arch/arm64/crypto/aes-ce-glue.c
+++ arch/arm64/crypto/aes-ce-glue.c
@@ -51,6 +51,39 @@ static int num_rounds(struct crypto_aes_ctx *ctx)
 static void aes_cipher_encrypt(struct crypto_tfm *tfm, u8 dst[], u8 const src[])
 {
 	struct crypto_aes_ctx *ctx = crypto_tfm_ctx(tfm);
+<<<<<<< HEAD:arch/arm64/crypto/aes-ce-glue.c
+=======
+	struct aes_block *out = (struct aes_block *)dst;
+	struct aes_block const *in = (struct aes_block *)src;
+	void *dummy0;
+	int dummy1;
+
+	kernel_neon_begin_partial(4);
+
+	__asm__("	ld1	{v0.16b}, %[in]			;"
+		"	ld1	{v1.4s}, [%[key]], #16		;"
+		"	cmp	%w[rounds], #10			;"
+		"	bmi	0f				;"
+		"	bne	3f				;"
+		"	mov	v3.16b, v1.16b			;"
+		"	b	2f				;"
+		"0:	mov	v2.16b, v1.16b			;"
+		"	ld1	{v3.4s}, [%[key]], #16		;"
+		"1:	aese	v0.16b, v2.16b			;"
+		"	aesmc	v0.16b, v0.16b			;"
+		"2:	ld1	{v1.4s}, [%[key]], #16		;"
+		"	aese	v0.16b, v3.16b			;"
+		"	aesmc	v0.16b, v0.16b			;"
+		"3:	ld1	{v2.4s}, [%[key]], #16		;"
+		"	subs	%w[rounds], %w[rounds], #3	;"
+		"	aese	v0.16b, v1.16b			;"
+		"	aesmc	v0.16b, v0.16b			;"
+		"	ld1	{v3.4s}, [%[key]], #16		;"
+		"	bpl	1b				;"
+		"	aese	v0.16b, v2.16b			;"
+		"	eor	v0.16b, v0.16b, v3.16b		;"
+		"	st1	{v0.16b}, %[out]		;"
+>>>>>>> 8ed0b23... UPSTREAM: crypto: arm64/aes-ce-cipher - match round key endianness with generic code:arch/arm64/crypto/aes-ce-cipher.c
 
 	if (!may_use_simd()) {
 		__aes_arm64_encrypt(ctx->key_enc, dst, src, num_rounds(ctx));
@@ -66,10 +99,44 @@ static void aes_cipher_decrypt(struct crypto_tfm *tfm, u8 dst[], u8 const src[])
 {
 	struct crypto_aes_ctx *ctx = crypto_tfm_ctx(tfm);
 
+<<<<<<< HEAD:arch/arm64/crypto/aes-ce-glue.c
 	if (!may_use_simd()) {
 		__aes_arm64_decrypt(ctx->key_dec, dst, src, num_rounds(ctx));
 		return;
 	}
+=======
+	__asm__("	ld1	{v0.16b}, %[in]			;"
+		"	ld1	{v1.4s}, [%[key]], #16		;"
+		"	cmp	%w[rounds], #10			;"
+		"	bmi	0f				;"
+		"	bne	3f				;"
+		"	mov	v3.16b, v1.16b			;"
+		"	b	2f				;"
+		"0:	mov	v2.16b, v1.16b			;"
+		"	ld1	{v3.4s}, [%[key]], #16		;"
+		"1:	aesd	v0.16b, v2.16b			;"
+		"	aesimc	v0.16b, v0.16b			;"
+		"2:	ld1	{v1.4s}, [%[key]], #16		;"
+		"	aesd	v0.16b, v3.16b			;"
+		"	aesimc	v0.16b, v0.16b			;"
+		"3:	ld1	{v2.4s}, [%[key]], #16		;"
+		"	subs	%w[rounds], %w[rounds], #3	;"
+		"	aesd	v0.16b, v1.16b			;"
+		"	aesimc	v0.16b, v0.16b			;"
+		"	ld1	{v3.4s}, [%[key]], #16		;"
+		"	bpl	1b				;"
+		"	aesd	v0.16b, v2.16b			;"
+		"	eor	v0.16b, v0.16b, v3.16b		;"
+		"	st1	{v0.16b}, %[out]		;"
+
+	:	[out]		"=Q"(*out),
+		[key]		"=r"(dummy0),
+		[rounds]	"=r"(dummy1)
+	:	[in]		"Q"(*in),
+				"1"(ctx->key_dec),
+				"2"(num_rounds(ctx) - 2)
+	:	"cc");
+>>>>>>> 8ed0b23... UPSTREAM: crypto: arm64/aes-ce-cipher - match round key endianness with generic code:arch/arm64/crypto/aes-ce-cipher.c
 
 	kernel_neon_begin();
 	__aes_ce_decrypt(ctx->key_dec, dst, src, num_rounds(ctx));
@@ -104,7 +171,11 @@ int ce_aes_expandkey(struct crypto_aes_ctx *ctx, const u8 *in_key,
 		u32 *rki = ctx->key_enc + (i * kwords);
 		u32 *rko = rki + kwords;
 
+<<<<<<< HEAD:arch/arm64/crypto/aes-ce-glue.c
 		rko[0] = ror32(__aes_ce_sub(rki[kwords - 1]), 8) ^ rcon[i] ^ rki[0];
+=======
+		rko[0] = ror32(aes_sub(rki[kwords - 1]), 8) ^ rcon[i] ^ rki[0];
+>>>>>>> 8ed0b23... UPSTREAM: crypto: arm64/aes-ce-cipher - match round key endianness with generic code:arch/arm64/crypto/aes-ce-cipher.c
 		rko[1] = rko[0] ^ rki[1];
 		rko[2] = rko[1] ^ rki[2];
 		rko[3] = rko[2] ^ rki[3];
@@ -136,7 +207,17 @@ int ce_aes_expandkey(struct crypto_aes_ctx *ctx, const u8 *in_key,
 
 	key_dec[0] = key_enc[j];
 	for (i = 1, j--; j > 0; i++, j--)
+<<<<<<< HEAD:arch/arm64/crypto/aes-ce-glue.c
 		__aes_ce_invert(key_dec + i, key_enc + j);
+=======
+		__asm__("ld1	{v0.4s}, %[in]		;"
+			"aesimc	v1.16b, v0.16b		;"
+			"st1	{v1.4s}, %[out]	;"
+
+		:	[out]	"=Q"(key_dec[i])
+		:	[in]	"Q"(key_enc[j])
+		:		"v0","v1");
+>>>>>>> 8ed0b23... UPSTREAM: crypto: arm64/aes-ce-cipher - match round key endianness with generic code:arch/arm64/crypto/aes-ce-cipher.c
 	key_dec[i] = key_enc[0];
 
 	kernel_neon_end();
